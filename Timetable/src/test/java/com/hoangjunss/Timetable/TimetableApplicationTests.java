cffb4dc16577c89463d19601a057d75eb2d20e4c
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-13 16:02:57 +0700

    first commit

---
+++ Timetable/src/test/java/com/hoangjunss/Timetable/TimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangjunss.Timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
