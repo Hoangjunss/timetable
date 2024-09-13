a116078774b2a1c2aaa4d136df51a5f2a9775238
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-13 22:24:06 +0700

    add login web

--- Timetable/src/main/java/com/hoangjunss/Timetable/TimetableController.java
+++ Timetable/src/main/java/com/hoangjunss/Timetable/TimetableController.java
@@ -3,11 +3,16 @@ package com.hoangjunss.Timetable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/timetable")
@@ -22,7 +27,27 @@ public class TimetableController {
             // Truy cập trang đăng nhập
             webDriver.get("https://thongtindaotao.sgu.edu.vn/#/home");
 
-            // Kiểm tra nếu trang đã tải thành công bằng cách tìm một phần tử cụ thể
+            WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+            try {
+                // Đợi cho đến khi trường username có thể tương tác
+                WebElement usernameField = wait.until(ExpectedConditions.elementToBeClickable(By.name("username")));
+                usernameField.sendKeys(username);
+
+                // Đợi cho đến khi trường password có thể tương tác
+                WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(By.name("password"))); // Thay 'password' bằng ID thật của trường này
+                passwordField.sendKeys(password);
+
+                // Đợi cho đến khi nút đăng nhập có thể nhấn
+                WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.className("btn")));
+                loginButton.click();
+
+                // Chờ trang tải xong và kiểm tra kết quả đăng nhập nếu cần
+                Thread.sleep(3000);
+            }catch (Exception e) {
+               e.printStackTrace();
+               return "Error  while accessing the login page.";
+           }
 
 
         } catch (Exception e) {
